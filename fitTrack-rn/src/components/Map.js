fbe213d11606d4ab2fb6b33fb99832e8d4f83c43
“Ron <wheresronald0@icloud.com>
2021-11-17 15:22:19 -0500

    wired in constant mock data simulating user device movement

--- fitTrack-rn/src/components/Map.js
+++ fitTrack-rn/src/components/Map.js
@@ -1,29 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 
 import MapView, { Polyline } from "react-native-maps";
+import LocationContext from "../context/LocationContext";
 
 const Map = (props) => {
-  let points = [];
-  for (let i = 0; i < 10; i++) {
-    points.push({
-      latitude: 27.498474 + i * 0.001,
-      longitude: -82.491622 + i * 0.001,
-    });
+  const {
+    state: { currentLocation },
+  } = useContext(LocationContext);
+
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ margin: 175 }} />;
   }
+  console.log(currentLocation.coords);
 
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 27.498474,
-        longitude: -82.491622,
-        latitudeDelta: 0.1,
-        longitudeDelta: 0.1,
+        ...currentLocation.coords,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
       }}
-    >
-      <Polyline coordinates={points} />
-    </MapView>
+      region={{
+        ...currentLocation.coords,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+      }}
+    ></MapView>
   );
 };
 
