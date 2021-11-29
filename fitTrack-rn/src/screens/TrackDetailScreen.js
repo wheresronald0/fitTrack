54f5a0048baadd1c289a1165d8a6e325e825beb6
“Ron <wheresronald0@icloud.com>
2021-11-29 13:24:33 -0500

    added mapVIew and mapped data to show route

--- fitTrack-rn/src/screens/TrackDetailScreen.js
+++ fitTrack-rn/src/screens/TrackDetailScreen.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import MapView, { Polyline } from "react-native-maps";
 import LocationContext from "../context/LocationContext";
 
 const TrackDetailScreen = ({ route }) => {
@@ -9,15 +10,29 @@ const TrackDetailScreen = ({ route }) => {
 
   const track = state.trackList.find((t) => t._id === id);
   console.log(track);
+  const initialCoords = track.locations[0].coords;
 
   return (
     <View>
-      <Text>Track Detail Screen!</Text>
       <Text>{track.name}</Text>
+      <MapView
+        initialRegion={{
+          longitudeDelta: 0.01,
+          latitudeDelta: 0.01,
+          ...initialCoords,
+        }}
+        style={styles.map}
+      >
+        <Polyline coordinates={track.locations.map((loc) => loc.coords)} />
+      </MapView>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
 
 export default TrackDetailScreen;
